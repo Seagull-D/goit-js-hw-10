dc3d56cbcacd5ab263aec95d44745697eafcccee
Seagull-D <136006992+Seagull-D@users.noreply.github.com>
2024-11-21 20:41:20 +0000

    Deploying to gh-pages from @ Seagull-D/goit-js-hw-10@5c95c626b23e78fae336ed3ccb1f0265926d8374 🚀

---
+++ 1-timer.js
@@ -0,0 +1,5 @@
+import"./assets/styles-DD3qoKza.js";const e=document.querySelector(".container"),t=`<h1 class="main-title">
+     TimerS
+        </h1>
+        `;e.insertAdjacentHTML("beforeend",t);
+//# sourceMappingURL=1-timer.js.map
---
+++ 2-snackbar.js
@@ -0,0 +1,5 @@
+import"./assets/styles-DD3qoKza.js";const e=document.querySelector(".container"),t=`<h1 class="main-title">
+          SnackBAR
+        </h1>
+        `;e.insertAdjacentHTML("beforeend",t);
+//# sourceMappingURL=2-snackbar.js.map
