5c95c626b23e78fae336ed3ccb1f0265926d8374
Dmytro Chaika <djseagull90@gmail.com>
2024-11-21 21:41:02 +0100

    created project

---
+++ src/js/1-timer.js
@@ -0,0 +1,7 @@
+
+const timer = document.querySelector(".container")
+const mainTitle = `<h1 class="main-title">
+     TimerS
+        </h1>
+        `;
+timer.insertAdjacentHTML("beforeend", mainTitle)
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,6 @@
+const timer = document.querySelector(".container")
+const mainTitle = `<h1 class="main-title">
+          SnackBAR
+        </h1>
+        `;
+timer.insertAdjacentHTML("beforeend", mainTitle)
