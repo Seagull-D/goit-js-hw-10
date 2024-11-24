1300bc8e6f76f35317958af864fb98257b8d7847
Dmytro Chaika <djseagull90@gmail.com>
2024-11-24 10:52:04 +0100

    timer
    active btn

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,28 @@
+"use strict"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-const timer = document.querySelector(".container")
-const mainTitle = `<h1 class="main-title">
-     TimerS
-        </h1>
-        `;
-timer.insertAdjacentHTML("beforeend", mainTitle)
+const startBtn = document.querySelector("[data-start]")
+startBtn.disabled = true;
+console.log(startBtn)
+let userSelectedDate = ""
+
+flatpickr("#datetime-picker", {
+enableTime: true,
+time_24hr: true,
+defaultDate: new Date(),
+minuteIncrement: 1,
+        onClose(selectedDates) {
+                   
+                if (selectedDates[0] > Date.now()) {
+                        startBtn.disabled = false;
+                        userSelectedDate = selectedDates[0]
+                } else {
+                    
+                        window.alert("Please choose a date in the future")
+                      
+                }
+                
+console.log(userSelectedDate)
+        }
+});
