cd47e1fa41b6e0a25d51bd6cba28822ac3d02ccd
Seagull-D <136006992+Seagull-D@users.noreply.github.com>
2024-11-26 17:58:56 +0000

    Deploying to gh-pages from @ Seagull-D/goit-js-hw-10@c1878d17d086fbc4d31302f02bb6f5d37cac3a20 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,5 +1,5 @@
-import"./assets/styles-CL-p8ArJ.js";const e=document.querySelector(".container"),t=`<h1 class="main-title">
+import"./assets/styles-CL-p8ArJ.js";const t=document.querySelector(".container"),e=`<h1 class="main-title">
           SnackBAR
         </h1>
-        `;e.insertAdjacentHTML("beforeend",t);
+        `;t.insertAdjacentHTML("beforeend",e);document.querySelector("span[data-seconds]").textContent=`${currentSecond}`;document.querySelector("span[data-minutes]").textContent=`${currentMinutes}`;document.querySelector("span[data-hours]").textContent=`${currentHours}`;document.querySelector("span[data-days]").textContent=`${currentDays}`;
 //# sourceMappingURL=2-snackbar.js.map
