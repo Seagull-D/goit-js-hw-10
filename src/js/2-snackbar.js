6f8d1cd2fe1dd99edcce19cee690a68c8d27f5a3
Dmytro Chaika <djseagull90@gmail.com>
2024-11-26 23:09:32 +0100

    snak bar end

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,11 +1,60 @@
-const timer = document.querySelector(".container")
-const mainTitle = `<h1 class="main-title">
-          SnackBAR
-        </h1>
-        `;
-timer.insertAdjacentHTML("beforeend", mainTitle)
+"use strict"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const secondHTML = document.querySelector("span[data-seconds]").textContent = `${currentSecond}`
-                        const minuteHTML = document.querySelector("span[data-minutes]").textContent = `${currentMinutes}`
-                        const hourHTML = document.querySelector("span[data-hours]").textContent = `${currentHours}`
-                        const dayHTML = document.querySelector("span[data-days]").textContent = `${currentDays}`
+const form = document.querySelector("form")
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+})
+
+
+const input = document.querySelector("input[name=delay]")
+const button = document.querySelector("button")
+console.log(button)
+
+console.log(input);
+function checkSelected() {
+  return new Promise((resolve, reject) => { 
+    const changeBTN = document.querySelector('input[name="state"]:checked');
+    const delayValue = input.value;
+    input.value = ""
+    
+ switch (changeBTN.value) {
+      case "fulfilled":
+     setTimeout(() => {
+        
+          resolve(`✅ Fulfilled promise in ${delayValue}ms`);
+         
+        }, delayValue);
+     
+        break;
+
+      case "rejected":
+        setTimeout(() => {
+          reject(`❌ Rejected promise in ${delayValue}ms`);
+        }, delayValue);
+
+        break;
+
+      default:
+        reject("error");
+        break;
+    }
+  });
+}
+
+
+button.addEventListener("click", () => {
+ checkSelected()
+    .then((value) => iziToast.show({
+      message: `${value}`,
+      position: "topRight",
+      color:"green"
+})
+  )
+.catch((error) => iziToast.show({
+  message: `${error}`,
+  position: "topRight",
+  color:"red"
+}))})
+    
